183812c9ec946e3b16322c7aba972faaa563fb27
firubii <fruitymage@gmail.com>
2021-04-27 17:47:52 -0500

    Add project files.

---
+++ CndXML/CNDBin.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace CndXML
+{
+    public class CNDBin
+    {
+        public XData XData;
+
+        public string Name;
+        public string Type;
+        public List<CNDStruct> VisualSection;
+        public List<CNDStruct> RenderSection;
+
+        public CNDBin(string name, string type, Endianness endianness)
+        {
+            XData = new XData(endianness);
+            Name = name;
+            Type = type;
+            VisualSection = new List<CNDStruct>();
+            RenderSection = new List<CNDStruct>();
+        }
+
+        public CNDBin(string filePath)
+        {
+            using (EndianBinaryReader reader = new EndianBinaryReader(new FileStream(filePath, FileMode.Open, FileAccess.Read)))
+                Read(reader);
+        }
+
+        public void Read(EndianBinaryReader reader)
+        {
+            XData = new XData(reader);
+            if (reader.ReadUInt32() != 10100)
+                throw new InvalidDataException("Invalid CND Binary");
+
+            reader.BaseStream.Seek(0x20, SeekOrigin.Begin);
+            uint visOffs = reader.ReadUInt32();
+            uint unk1 = reader.ReadUInt32();
+            uint nameOffs = reader.ReadUInt32();
+            uint typeOffs = reader.ReadUInt32();
+            uint rndOffs = reader.ReadUInt32();
+
+            reader.BaseStream.Seek(nameOffs, SeekOrigin.Begin);
+            Name = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt32()));
+            reader.BaseStream.Seek(typeOffs, SeekOrigin.Begin);
+            Type = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt32()));
+
+            VisualSection = new List<CNDStruct>();
+            reader.BaseStream.Seek(visOffs, SeekOrigin.Begin);
+            uint visCount = reader.ReadUInt32();
+            for (int i = 0; i < visCount; i++)
+            {
+                reader.BaseStream.Seek(visOffs + 4 + (i * 4), SeekOrigin.Begin);
+                reader.BaseStream.Seek(reader.ReadUInt32(), SeekOrigin.Begin);
+                VisualSection.Add(new CNDStruct(reader));
+            }
+
+            RenderSection = new List<CNDStruct>();
+            reader.BaseStream.Seek(rndOffs, SeekOrigin.Begin);
+            uint rndCount = reader.ReadUInt32();
+            for (int i = 0; i < rndCount; i++)
+            {
+                reader.BaseStream.Seek(rndOffs + 4 + (i * 4), SeekOrigin.Begin);
+                reader.BaseStream.Seek(reader.ReadUInt32(), SeekOrigin.Begin);
+                RenderSection.Add(new CNDStruct(reader));
+            }
+        }
+    }
+
+    public class CNDStruct
+    {
+        public string Name;
+        public string Type;
+        public uint Flags;
+        public List<CNDGroup> Groups;
+
+        public CNDStruct(string name, string type, uint flags)
+        {
+            Name = name;
+            Type = type;
+            Flags = flags;
+            Groups = new List<CNDGroup>();
+        }
+
+        public CNDStruct(EndianBinaryReader reader)
+        {
+            long pos = reader.BaseStream.Position;
+            uint nameOffs = reader.ReadUInt32();
+            uint typeOffs = reader.ReadUInt32();
+            Flags = reader.ReadUInt32();
+            uint grpOffs = reader.ReadUInt32();
+
+            reader.BaseStream.Seek(nameOffs, SeekOrigin.Begin);
+            Name = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt32()));
+            reader.BaseStream.Seek(typeOffs, SeekOrigin.Begin);
+            Type = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt32()));
+
+            Groups = new List<CNDGroup>();
+            reader.BaseStream.Seek(grpOffs, SeekOrigin.Begin);
+            uint grpCount = reader.ReadUInt32();
+            for (int i = 0; i < grpCount; i++)
+            {
+                reader.BaseStream.Seek(grpOffs + 4 + (i * 4), SeekOrigin.Begin);
+                uint dataOffs = reader.ReadUInt32();
+                reader.BaseStream.Seek(dataOffs + 4, SeekOrigin.Begin);
+
+                //Check version based on group data offset
+                //Because the file doesn't have a version indicator in the header, we have to do this to
+                //detect the version instead.
+
+                //If group offset is 8 bytes away from the given offset, treat the file as the KF2 format.
+                //If not, treat the file as the KSA/SKC format.
+                if (reader.ReadUInt32() == dataOffs + 8)
+                {
+                    reader.BaseStream.Seek(dataOffs, SeekOrigin.Begin);
+                    Groups.Add(new CNDGroup(reader));
+                }
+                else
+                {
+                    reader.BaseStream.Seek(dataOffs, SeekOrigin.Begin);
+                    if (Groups.Count == 0)
+                        Groups.Add(new CNDGroup(""));
+
+                    Groups[0].Variables.Add(new CNDVariable(reader));
+                }
+            }
+        }
+    }
+
+    public class CNDGroup
+    {
+        public string Name;
+        public List<CNDVariable> Variables;
+
+        public CNDGroup(string name)
+        {
+            Name = name;
+            Variables = new List<CNDVariable>();
+        }
+
+        public CNDGroup(EndianBinaryReader reader)
+        {
+            uint nameOffs = reader.ReadUInt32();
+            uint dataOffs = reader.ReadUInt32();
+
+            reader.BaseStream.Seek(nameOffs, SeekOrigin.Begin);
+            Name = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt32()));
+
+            Variables = new List<CNDVariable>();
+            reader.BaseStream.Seek(dataOffs, SeekOrigin.Begin);
+            uint varCount = reader.ReadUInt32();
+            for (int i = 0; i < varCount; i++)
+            {
+                reader.BaseStream.Seek(dataOffs + 4 + (i * 4), SeekOrigin.Begin);
+                reader.BaseStream.Seek(reader.ReadUInt32(), SeekOrigin.Begin);
+                Variables.Add(new CNDVariable(reader));
+            }
+        }
+    }
+
+    public class CNDVariable
+    {
+        public string Name;
+        public string Type;
+        public uint Flags;
+        public byte[] Data;
+
+        public CNDVariable(string name, string type, uint flags, object value)
+        {
+            Name = name;
+            Type = type;
+            Flags = flags;
+            SetValue(value);
+        }
+
+        public CNDVariable(EndianBinaryReader reader)
+        {
+            long pos = reader.BaseStream.Position;
+
+            uint nameOffs = reader.ReadUInt32();
+            uint typeOffs = reader.ReadUInt32();
+            Flags = reader.ReadUInt32();
+            int dataSize = reader.ReadInt32();
+
+            reader.BaseStream.Seek(nameOffs, SeekOrigin.Begin);
+            Name = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt32()));
+            reader.BaseStream.Seek(typeOffs, SeekOrigin.Begin);
+            Type = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt32()));
+
+            reader.BaseStream.Seek(pos + 0x10, SeekOrigin.Begin);
+            if (Type != "String")
+                Data = reader.ReadBytes(dataSize);
+            else
+                Data = reader.ReadBytes(reader.ReadInt32());
+        }
+
+        public object GetValue()
+        {
+            switch (Type)
+            {
+                case "Int":
+                    {
+                        return BitConverter.ToInt32(Data, 0);
+                    }
+                case "Float":
+                    {
+                        return BitConverter.ToSingle(Data, 0);
+                    }
+                case "Bool":
+                    {
+                        return Convert.ToBoolean(BitConverter.ToInt32(Data, 0));
+                    }
+                case "Color4":
+                    {
+                        return Color.FromArgb(Data[3], Data[0], Data[1], Data[2]);
+                    }
+                case "Vec3":
+                    {
+                        return new float[3] {
+                            BitConverter.ToSingle(Data, 0),
+                            BitConverter.ToSingle(Data, 4),
+                            BitConverter.ToSingle(Data, 8),
+                        };
+                    }
+                case "String":
+                    {
+                        return Encoding.UTF8.GetString(Data);
+                    }
+                default:
+                    {
+                        throw new NotImplementedException("Unknown CND Variable type");
+                    }
+            }
+        }
+
+        public void SetValue(object value)
+        {
+            switch (Type)
+            {
+                case "Int":
+                    {
+                        Data = BitConverter.GetBytes((int)value);
+                        break;
+                    }
+                case "Float":
+                    {
+                        Data = BitConverter.GetBytes((float)value);
+                        break;
+                    }
+                case "Bool":
+                    {
+                        Data = BitConverter.GetBytes(Convert.ToInt32((bool)value));
+                        break;
+                    }
+                case "Color4":
+                    {
+                        Color c = (Color)value;
+                        Data = new byte[] { c.R, c.G, c.B, c.A };
+                        break;
+                    }
+                case "Vec3":
+                    {
+                        float[] v = value as float[];
+                        Data = new byte[12];
+                        Buffer.BlockCopy(BitConverter.GetBytes(v[0]), 0, Data, 0, 4);
+                        Buffer.BlockCopy(BitConverter.GetBytes(v[1]), 0, Data, 4, 4);
+                        Buffer.BlockCopy(BitConverter.GetBytes(v[2]), 0, Data, 8, 4);
+                        break;
+                    }
+                case "String":
+                    {
+                        Data = Encoding.UTF8.GetBytes(value as string);
+                        break;
+                    }
+                default:
+                    {
+                        throw new NotImplementedException("Unknown CND Variable type");
+                    }
+            }
+        }
+    }
+}
---
+++ CndXML/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Drawing;
+
+namespace CndXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (File.Exists(args[0]))
+                {
+                    Console.WriteLine("Reading CND Binary...");
+
+                    CNDBin cnd = new CNDBin(args[0]);
+
+                    Console.WriteLine("Creating XML file...");
+
+                    XmlDocument xml = new XmlDocument();
+                    xml.AppendChild(xml.CreateXmlDeclaration("1.0", "utf-8", ""));
+
+                    XmlElement root = xml.CreateElement("CND");
+                    root.SetAttribute("name", cnd.Name);
+                    root.SetAttribute("type", cnd.Type);
+
+                    XmlElement visSection = xml.CreateElement("VisualSection");
+                    for (int i = 0; i < cnd.VisualSection.Count; i++)
+                    {
+                        XmlElement cndStruct = xml.CreateElement("Struct");
+                        cndStruct.SetAttribute("name", cnd.VisualSection[i].Name);
+                        cndStruct.SetAttribute("type", cnd.VisualSection[i].Type);
+                        cndStruct.SetAttribute("flags", cnd.VisualSection[i].Flags.ToString());
+
+                        for (int g = 0; g < cnd.VisualSection[i].Groups.Count; g++)
+                        {
+                            XmlElement group = xml.CreateElement("Group");
+                            group.SetAttribute("name", cnd.VisualSection[i].Groups[g].Name);
+
+                            for (int v = 0; v < cnd.VisualSection[i].Groups[g].Variables.Count; v++)
+                            {
+                                XmlElement variable = xml.CreateElement(cnd.VisualSection[i].Groups[g].Variables[v].Type);
+                                variable.SetAttribute("name", cnd.VisualSection[i].Groups[g].Variables[v].Name);
+
+                                if (cnd.VisualSection[i].Groups[g].Variables[v].Type == "Color4")
+                                {
+                                    Color c = (Color)cnd.VisualSection[i].Groups[g].Variables[v].GetValue();
+                                    XmlElement cR = xml.CreateElement("R");
+                                    cR.InnerText = c.R.ToString();
+                                    XmlElement cG = xml.CreateElement("G");
+                                    cG.InnerText = c.G.ToString();
+                                    XmlElement cB = xml.CreateElement("B");
+                                    cB.InnerText = c.B.ToString();
+                                    XmlElement cA = xml.CreateElement("A");
+                                    cA.InnerText = c.A.ToString();
+
+                                    variable.AppendChild(cR);
+                                    variable.AppendChild(cG);
+                                    variable.AppendChild(cB);
+                                    variable.AppendChild(cA);
+                                }
+                                else if (cnd.VisualSection[i].Groups[g].Variables[v].Type == "Vec3")
+                                {
+                                    float[] vec = cnd.VisualSection[i].Groups[g].Variables[v].GetValue() as float[];
+                                    XmlElement vX = xml.CreateElement("X");
+                                    vX.InnerText = vec[0].ToString();
+                                    XmlElement vY = xml.CreateElement("Y");
+                                    vY.InnerText = vec[1].ToString();
+                                    XmlElement vZ = xml.CreateElement("Z");
+                                    vZ.InnerText = vec[2].ToString();
+
+                                    variable.AppendChild(vX);
+                                    variable.AppendChild(vY);
+                                    variable.AppendChild(vZ);
+                                }
+                                else
+                                    variable.InnerText = cnd.VisualSection[i].Groups[g].Variables[v].GetValue().ToString();
+
+                                group.AppendChild(variable);
+                            }
+
+                            cndStruct.AppendChild(group);
+                        }
+
+                        visSection.AppendChild(cndStruct);
+                    }
+
+                    XmlElement rndSection = xml.CreateElement("RenderSection");
+                    for (int i = 0; i < cnd.RenderSection.Count; i++)
+                    {
+                        XmlElement cndStruct = xml.CreateElement("Struct");
+                        cndStruct.SetAttribute("name", cnd.RenderSection[i].Name);
+                        cndStruct.SetAttribute("type", cnd.RenderSection[i].Type);
+                        cndStruct.SetAttribute("flags", cnd.RenderSection[i].Flags.ToString());
+
+                        for (int g = 0; g < cnd.RenderSection[i].Groups.Count; g++)
+                        {
+                            XmlElement group = xml.CreateElement("Group");
+                            group.SetAttribute("name", cnd.RenderSection[i].Groups[g].Name);
+
+                            for (int v = 0; v < cnd.RenderSection[i].Groups[g].Variables.Count; v++)
+                            {
+                                XmlElement variable = xml.CreateElement(cnd.RenderSection[i].Groups[g].Variables[v].Type);
+                                variable.SetAttribute("name", cnd.RenderSection[i].Groups[g].Variables[v].Name);
+
+                                if (cnd.RenderSection[i].Groups[g].Variables[v].Type == "Color4")
+                                {
+                                    Color c = (Color)cnd.RenderSection[i].Groups[g].Variables[v].GetValue();
+                                    XmlElement cR = xml.CreateElement("R");
+                                    cR.InnerText = c.R.ToString();
+                                    XmlElement cG = xml.CreateElement("G");
+                                    cG.InnerText = c.G.ToString();
+                                    XmlElement cB = xml.CreateElement("B");
+                                    cB.InnerText = c.B.ToString();
+                                    XmlElement cA = xml.CreateElement("A");
+                                    cA.InnerText = c.A.ToString();
+
+                                    variable.AppendChild(cR);
+                                    variable.AppendChild(cG);
+                                    variable.AppendChild(cB);
+                                    variable.AppendChild(cA);
+                                }
+                                else if (cnd.RenderSection[i].Groups[g].Variables[v].Type == "Vec3")
+                                {
+                                    float[] vec = cnd.RenderSection[i].Groups[g].Variables[v].GetValue() as float[];
+                                    XmlElement vX = xml.CreateElement("X");
+                                    vX.InnerText = vec[0].ToString();
+                                    XmlElement vY = xml.CreateElement("Y");
+                                    vY.InnerText = vec[1].ToString();
+                                    XmlElement vZ = xml.CreateElement("Z");
+                                    vZ.InnerText = vec[2].ToString();
+
+                                    variable.AppendChild(vX);
+                                    variable.AppendChild(vY);
+                                    variable.AppendChild(vZ);
+                                }
+                                else
+                                    variable.InnerText = cnd.RenderSection[i].Groups[g].Variables[v].GetValue().ToString();
+
+                                group.AppendChild(variable);
+                            }
+
+                            cndStruct.AppendChild(group);
+                        }
+
+                        rndSection.AppendChild(cndStruct);
+                    }
+
+                    root.AppendChild(visSection);
+                    root.AppendChild(rndSection);
+
+                    xml.AppendChild(root);
+
+                    xml.Save(Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]) + ".xml");
+
+                    Console.WriteLine($"Written XML file to {Path.GetDirectoryName(args[0])}\\{Path.GetFileNameWithoutExtension(args[0])}.xml");
+                }
+                else
+                {
+                    Console.WriteLine("Error: File does not exist!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: CndXML.exe <cndbin path>");
+            }
+        }
+    }
+}
